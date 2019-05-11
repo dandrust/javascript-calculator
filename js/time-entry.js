e365fbe63ee7efe33b8771c81c27066bb470b41b
Dan D <dandrust@gmail.com>
2019-05-11 18:37:44 -0400

    Add vue

--- js/time-entry.js
+++ js/time-entry.js
@@ -1,4 +1,4 @@
-var debug = true;
+ debug = true;
 var debugVal;
 
 var debug = function(value) {
@@ -8,28 +8,64 @@ var debug = function(value) {
 
 // DRAG AND DROP
 $(document).ready(function(){
+  // Be careful because text is always draggable!
 
-  $('.list-group-item').attr('draggable', true);
-  $('.list-group-item').on('dragstart', function(e) {
-    debugVal = e.target;
-    e.originalEvent.dataTransfer.setData("text", e.target.innerText);
-  })
-
-  $('.agenda-block').on('dragenter', function(e) {
-    $(this).addClass('bg-primary')
-  })
-  $('.agenda-block').on('dragexit', function(e) {
-    $(this).removeClass('bg-primary')
-  })
-  $('.agenda-block').on('dragover', function(e) {
-    e.preventDefault();
-  })
-
-  $('.agenda-block').on('drop', function(e) {
-    e.preventDefault();
-    var data = e.originalEvent.dataTransfer.getData("text");
-    $(e.target).html(data);
-  })
 });
 
 // RESIZE
+
+$(document).ready(function(){
+  $('.drag-handle').on('mousedown', initializeResize);
+});
+
+var writeBoxPercents = function() {
+  $container = $('#slider-container');
+  containerHeight = $container.height();
+  $container.children().each(function(index){
+    $child = $(this);
+    height = $child.height();
+    $child.find('.box-label').html(parseInt(100 * height / containerHeight));
+  })
+}
+
+var initializeResize = function(e) {
+  debug("resizing!")
+  $(window).on('mousemove', startResize.bind(null, $(e.target).parent().parent()))
+  $(window).on('mouseup', stopResize.bind(null, $(e.target).parent().parent()))
+}
+
+var stopResize = function(e) {
+  $(window).off('mousemove')
+  $(window).off('mouseup')
+}
+
+var startResize = function($target, e) {
+  $sibling = $target.parent().next();
+  debugVal = $target;
+
+  targetHeight = $target.outerHeight();
+  siblingSnapTarget = $sibling.offset()['top'] + $sibling.outerHeight();
+  siblingSnapBoundary = siblingSnapTarget - ( $sibling.outerHeight() * 0.15 );
+  //siblingHeight = $sibling.height();
+  //combinedHeight = targetHeight + siblingHeight;
+  
+  if (e.originalEvent.clientY >= siblingSnapBoundary) {
+    debug('boundary crossed!')
+    newHeight = siblingSnapBoundary - $target.parent().offset()['top'];
+    $target.css('height', newHeight + 'px');
+  }
+
+  //newTargetHeight = (e.originalEvent.clientY - $target.offset()['top'])
+  //$target.css('height', newTargetHeight + 'px');
+  
+  //$sibling.css('height', combinedHeight - newTargetHeight + 'px');
+  //writeBoxPercents()
+}
+
+var writeDisplay = function() {
+  $('#display').html(total);
+}
+
+var initializeDisplay = function() {
+  writeDisplay();
+}
